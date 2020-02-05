00e18294ce7cb63d58df95aba37eaed790b4c710
cecilialuna11 <32347620+cecilialuna11@users.noreply.github.com>
2020-02-05 12:10:42 -0800

    Added code for humidity reader.

--- stem_day_iot.ino
+++ stem_day_iot.ino
@@ -7,6 +7,12 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
+#include <Wire.h>
+
+// This code is designed to work with the SI7006-A20_I2CS I2C Mini Module available from ControlEverything.com.
+// https://www.controleverything.com/content/Humidity?sku=SI7006-A20_I2CS#tabs-0-product_tabset-2
+// SI7006-A20 I2C address is 0x40(64)
+#define Addr 0x40
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
@@ -83,6 +89,25 @@ void setup()
   Blynk.begin(auth, ssid, pass);
 
   timer.setInterval(1 * 1000L, myTimerEvent);
+
+/********Humidity Code Starts************/
+  pinMode(21, OUTPUT);
+  pinMode(22,  OUTPUT);
+
+  digitalWrite(21, HIGH);  // Power
+  digitalWrite(22,  LOW); // Ground
+  
+  Wire.begin(23, 19);
+
+  
+  Serial.begin(115200);
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Stop I2C transmission
+  Wire.endTransmission();
+  delay(300);
+/********Humidity Code Ends ************/
 }
 
 void loop()
@@ -90,7 +115,69 @@ void loop()
   Blynk.run();
   timer.run();
   int light = analogRead(4);
+
+/********Humidity Code Starts************/
+    uint8_t data[2] = {0};
   
+    // Start I2C transmission
+    Wire.beginTransmission(Addr);
+    // Send humidity measurement command, NO HOLD MASTER
+    Wire.write(0xF5);
+    // Stop I2C transmission
+    Wire.endTransmission();
+    delay(500);
+    
+    // Request 2 bytes of data
+    Wire.requestFrom(Addr, 2);
+
+    // Read 2 bytes of data
+    // humidity msb, humidity lsb 
+    if(Wire.available() == 2)
+    {
+      data[0] = Wire.read();
+      data[1] = Wire.read();
+    }
+    
+    // Convert the data
+    float humidity  = ((data[0] * 256.0) + data[1]);
+    humidity = ((125 * humidity) / 65536.0) - 6;
+
+    // Start I2C transmission
+    Wire.beginTransmission(Addr);
+    // Send temperature measurement command, NO HOLD MASTER
+    Wire.write(0xF3);
+    // Stop I2C transmission
+    Wire.endTransmission();
+    delay(500);
+    
+    // Request 2 bytes of data
+    Wire.requestFrom(Addr, 2);
+
+    // Read 2 bytes of data
+    // temp msb, temp lsb
+    if(Wire.available() == 2)
+    {
+      data[0] = Wire.read();
+      data[1] = Wire.read();
+    }
+
+    // Convert the data
+    float temp  = ((data[0] * 256.0) + data[1]);
+    float ctemp = ((175.72 * temp) / 65536.0) - 46.85;
+    float ftemp = ctemp * 1.8 + 32;
+    
+    // Output data to serial monitor
+    Serial.print("Relative humidity : ");
+    Serial.print(humidity);
+    Serial.println(" % RH");
+    Serial.print("Temperature in Celsius : ");
+    Serial.print(ctemp);
+    Serial.println(" C");
+    Serial.print("Temperature in Fahrenheit : ");
+    Serial.print(ftemp);
+    Serial.println(" F");
+    delay(500);
+/********Humidity Code Ends************/
 }
 
 
